b402b7e4190265a9d4de1aa5b44de588a48903b5
mirantel <magi.mirantel@gmail.com>
2013-02-26 13:44:05 +0200

    add js

--- js/common.js
+++ js/common.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
 
 	$('.category__selected').click(function() {
 		if ($(this).hasClass('category__selected_open')){
-				$(this).next('.category__drop').fadeOut('fast');
+				$(this).next('.category__drop').hide();
 				$(this).removeClass('category__selected_open');
 			}
 			else{
-				$(this).next('.category__drop').fadeIn('fast');
+				$(this).next('.category__drop').show();
 				$(this).addClass('category__selected_open');
 			}
 		return false;
@@ -25,8 +25,26 @@ $(document).ready(function() {
 		var temporary = $('.category__selected span').html();
 		$('.category__selected span').html($(this).html());
 		$(this).html(temporary);
-		$('.category__drop').fadeOut('fast');
+		$('.category__drop').hide();
 		$('.category__selected').removeClass('category__selected_open');
 		return false;
+	});
+	$('.menu .general').click(function() {
+		if ($(this).parent().parent().hasClass('open')) {
+			$(this).parent().parent().removeClass('open');
+			$(this).siblings('li').hide();
+		}
+		else{
+			$(this).parent().parent().addClass('open');
+			$(this).siblings('li').show();
+		}
+	});
+
+	jQuery(document).click( function(event){
+		if( $(event.target).closest(".menu .general").length )
+		return;
+		jQuery(".menu .general").siblings('li').hide();
+		$('.menu').removeClass('open');
+		event.stopPropagation();
 	});
 });
